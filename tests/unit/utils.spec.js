ddc6f4252f29314a07074008279499bbb2b07e4a
Jeffery Liang <jefferyliang@JefferydeMBP.lan>
2017-10-23 16:04:41 +0800

    add the utils.js test case

--- tests/unit/utils.spec.js
+++ tests/unit/utils.spec.js
@@ -1,13 +1,19 @@
 'use strict'
-const expect = require('chai').expect
+const chai = require('chai')
+const expect = chai.expect
+const spies = require('chai-spies')
 const assert = require('assert')
 const httpMocks = require('node-mocks-http')
 const utils = require('../../lib/utils')
 
 describe('Testing Utils', () => {
-  let ctx, next, date, method
+  let ctx, next, data, method
 
-  ctx = {}
+  ctx = {
+    request: {},
+    response: {},
+    throw: function () {}
+  }
 
   let response = {
     success: {
@@ -28,7 +34,13 @@ describe('Testing Utils', () => {
     ctx.response = httpMocks.createResponse()
 
     next = function () {
-      ctx.body = response.success
+      ctx.response.send(response.success)
+    }
+
+    ctx.throw = function (status, response) {
+      ctx.status = status
+      ctx.body = response
+      return ctx
     }
 
     done()
@@ -45,5 +57,70 @@ describe('Testing Utils', () => {
       assert(utils.checkProperties, true)
     })
 
+    it('They should be functions', () => {
+      expect(utils.deny).to.be.a('function')
+      expect(utils.checkProperties).to.be.a('function')
+      expect(utils.validate).to.be.a('function')
+      expect(utils.whenGlobAndActionAllow).to.be.a('function')
+      expect(utils.whenGlobAndActionDeny).to.be.a('function')
+      expect(utils.whenIsArrayMethod).to.be.a('function')
+      expect(utils.whenResourceAndMethodGlob).to.be.a('function')
+    })
+
   })
+
+  describe('utils.deny', () => {
+
+    it('should return default message when called with status', () => {
+      utils.deny(ctx, 403)
+      data = ctx.body
+      assert(data, true)
+      expect(data).to.be.an('object')
+      assert.deepEqual(data, response.restricted)
+    })
+
+    it('Should return custom message when called with message', () => {
+      utils.deny(ctx, 450, 'Role not found')
+      data = ctx.body
+      assert(data, true)
+      expect(data).to.be.an('object')
+      assert.deepEqual(data, {
+        status: 'Access denied',
+        success: false,
+        message: 'Role not found'
+      })
+    })
+
+    it('should return custom error when called with custom error message', () => {
+      chai.use(spies)
+      let customErrorResponse = {
+        status: 'Access Denied',
+        message: 'You are not authorized to access this resource'
+      }
+
+      utils.deny(ctx, 403, null, customErrorResponse)
+      data = ctx.body
+      assert(data, true)
+      expect(data).to.be.an('object')
+      expect(ctx.status).to.equal(403)
+      assert.deepEqual(data, customErrorResponse)
+    })
+
+  })
+
+  // describe('Utils.whenGlobAndActionAllow', () => {
+
+  //   context('When the Methods are a string', () => {
+      
+  //     it('should call next when method is string and "*"', () => {
+  //       utils.whenGlobAndActionAllow(ctx, next, null, '*')
+  //       data = ctx.body
+  //       assert(data, true)
+  //       expect(data).to.be.an('object')
+  //       assert.deepEqual(data, response.success)
+  //     })
+
+  //   })
+
+  // })
 })
