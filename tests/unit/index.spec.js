702bc7f8488d4f8881a50a2e34d38f7ec3dd3167
Jeffery Liang <lzh.jeffery@gmail.com>
2018-07-13 15:52:54 +0800

    update unit test code and upgrade the middleware

--- tests/unit/index.spec.js
+++ tests/unit/index.spec.js
@@ -1,26 +1,26 @@
-'use strict'
-const expect = require('chai').expect
-const assert = require('assert')
-const acl = require('../../')
+'use strict';
+const expect = require('chai').expect;
+const assert = require('assert');
+const acl = require('../../');
 
-describe('Check Koa 2 acl export', () => {
+describe('Koa acl export', () => {
   context('Check if they exist', () => {
-    it('All method should be defined', (done) => {
-      assert(acl, true)
-      assert(acl.config, true)
-      assert(acl.authorize, true)
-      assert(acl.authorize.unless, true)
-      done()
-    })
-  })
+    it('All method should be defined', done => {
+      assert(acl, true);
+      assert(acl.config, true);
+      assert(acl.authorize, true);
+      assert(acl.authorize.unless, true);
+      done();
+    });
+  });
 
   context('Check for the type', () => {
-    it('All properties should be functions', (done) => {
-      expect(acl).to.be.an('object')
-      expect(acl.config).to.be.a('function')
-      expect(acl.authorize).to.be.a('function')
-      expect(acl.authorize.unless).to.be.a('function')
-      done()
-    })
-  })
-})
+    it('All properties should be functions', done => {
+      expect(acl).to.be.an('object');
+      expect(acl.config).to.be.a('function');
+      expect(acl.authorize).to.be.a('function');
+      expect(acl.authorize.unless).to.be.a('function');
+      done();
+    });
+  });
+});
